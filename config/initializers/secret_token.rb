15c8a8b55ba830f889c90d4a9f0b5ac0282ce7ab
Jinxy05 <julianhurley11@gmail.com>
2014-03-03 03:50:39 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bloodbikeswales::Application.config.secret_key_base = 'c7d0bcb9a4b2b7a76b45bda8e82e0f71fcb2e36eb4088e06cf707f4c8099e581d2f3d3bd1bfe4d6c369c951bc7a4184e39405d1327e1fd6289626321a32354a8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bloodbikeswales::Application.config.session_store :cookie_store, key: '_bloodbikeswales_session'
