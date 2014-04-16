384491836b4ba13eaed6c973abc9bb2d1da33a6f
Jinxy05 <julianhurley11@gmail.com>
2014-04-16 17:30:49 +0100

    refactor assimilate method

--- lib/ruby_extension/string.rb
+++ lib/ruby_extension/string.rb
@@ -1,13 +1,22 @@
 class String
 
-	def is_uppercase?
+	def is_all_uppercase?
 		self == self.upcase 	
 	end
 
-	 def is_lowercase?
+	def is_all_lowercase?
  		self == self.downcase
  	end
 
+ 	def assimilate(outstring)
+ 		template = self.chars[0..outstring.length - 1]
+
+ 		template.each_with_index do |char, index|
+			char.is_all_uppercase? ? outstring[index] = outstring[index].upcase : outstring[index] = outstring[index].downcase
+ 		end
+
+ 		outstring
+ 	end
 
 
 end
