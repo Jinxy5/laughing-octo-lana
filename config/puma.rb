5ea15b8b78f1f19235ed33aabfcf46a6ad9855f9
Jinxy05 <julianhurley11@gmail.com>
2014-05-12 00:29:19 +0100

    tweak puma conf for logging reasons

--- config/puma.rb
+++ config/puma.rb
@@ -5,6 +5,7 @@ environment "production"
 daemonize true
 drain_on_shutdown true
 
-bind  "unix://" + path + "socket/puma.sock"
 pidfile path + "pid/puma.pid"
 state_path path + "pid/puma.state"
+
+bind  "unix://" + path + "socket/puma.sock" # bind needs to be set at the end for logging reasons?
