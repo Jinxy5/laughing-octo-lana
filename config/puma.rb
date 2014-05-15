812bacd9412d6830a5c9fa8816ae463f07576710
Jinxy05 <julianhurley11@gmail.com>
2014-05-15 17:28:33 +0100

    tweak git ignore

--- config/puma.rb
+++ config/puma.rb
@@ -8,4 +8,8 @@ drain_on_shutdown true
 pidfile path + "pid/puma.pid"
 state_path path + "pid/puma.state"
 
+stdout_redirect
+"#{application_path}/log/puma-#{railsenv}.stdout.log",
+"#{application_path}/log/puma-#{railsenv}.stderr.log"
+
 bind  "unix://" + path + "socket/puma.sock" # bind needs to be set at the end for logging reasons?
