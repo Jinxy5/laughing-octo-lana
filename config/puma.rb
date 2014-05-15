3e07799b1de83383846fff371fd8d82f64814344
Jinxy05 <julianhurley11@gmail.com>
2014-05-15 17:41:18 +0100

    tweak

--- config/puma.rb
+++ config/puma.rb
@@ -9,7 +9,7 @@ pidfile path + "pid/puma.pid"
 state_path path + "pid/puma.state"
 
 stdout_redirect
-"#{path}/log/puma-#{RAILS.env}.stdout.log"
-"#{path}/log/puma-#{RAILS.env}.stderr.log"
+"#{path}/log/puma-production.stdout.log"
+"#{path}/log/puma-production.stderr.log"
 
 bind  "unix://" + path + "socket/puma.sock" # bind needs to be set at the end for logging reasons?
